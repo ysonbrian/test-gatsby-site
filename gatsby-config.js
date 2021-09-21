81c9922c2168fc7552b587d63b70b6d41291f1cc
ysonbrian <znlsakxls@gmail.com>
2021-09-21 17:49:15 +0900

    Finished Gatsby tu p6

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,16 @@ module.exports = {
     title: 'ydblog',
   },
   plugins: [
+    'gatsby-plugin-gatsby-cloud',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/blog/`,
       },
     },
+    'gatsby-plugin-mdx',
   ],
 };
---
+++ src/pages/blog/index.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Link, graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import Layout from '../../components/layout';
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {/* {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))} */}
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <Link to={`/blog/${node.slug}`}>
+            <h2>{node.frontmatter.title}</h2>
+          </Link>
+          <p>Posted: {node.frontmatter.date}</p>
+        </article>
+      ))}
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: " YYYY-MMMM-D")
+          title
+        }
+        id
+        slug
+      }
+    }
+  }
+`;
+
+export default BlogPage;
